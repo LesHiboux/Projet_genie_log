daa5184507ba9b418bc6e2b7082c975e15315f2a
MartinAlexandreCERI <martinalexandre0496@gmail.com>
2017-04-21 17:15:12 +0200

    Update Map.h

--- Projet_Genie_Log_Reorganized/include/Map.h
+++ Projet_Genie_Log_Reorganized/include/Map.h
@@ -13,41 +13,41 @@ class Map
 	int** mapApparence;	/*!< Tableau utilisé pour l'apparence de la map */
 
 	public:
-		//*! Constructeur de map
+		//! Constructeur de map
 		/*! Récuperer les informations du fichier map.db pour créer la map */
 		Map();
 	
-		//*! Interface du tableau d'entitées
+		//! Interface du tableau d'entitées
 		/*! Retourne le tableau des entitées */
 		int** getMapEntite();
 	
-		//*! Interface du tableau de decor
+		//! Interface du tableau de decor
 		/*! Retourne le tableau représentant le decor */
 		int** getMapApparence();
 	
-		//*! Interface des dimensions de la map
+		//! Interface des dimensions de la map
 		/*! Retourne une paire représentant la taille de la map <largeur, longueur> */
 		std::pair<int,int> getTaille();
 	
-		//*! Interface de la position du joueur
+		//! Interface de la position du joueur
 		/*! Retourne une paire représentant les coordonnées du personnage <x, y> */
 		std::pair<int,int> getPosition();
 	
-		//*! Fonction servant à deplacer le personnage
+		//! Fonction servant à deplacer le personnage
 		/*!
 			/parametre x: un char représentant la direction du déplacement ('z','q','s' ou 'd')
 		*/
 		bool deplacer(char x);
 	
-		//*! Fonction ressortant le nombre de monstres restants
+		//! Fonction ressortant le nombre de monstres restants
 		/*! Calculé à partir du tableau d'entitées */
 		int mobRestants();
 	
-		//*! Fonction indiquant la présence ou non d'un monstre
+		//! Fonction indiquant la présence ou non d'un monstre
 		/*! Se base sur la position du joueur */
 		bool verif_monstre();
 	
-		//*! Fonction servant à enlever un monstre
+		//! Fonction servant à enlever un monstre
 		/*! L'enlève du tableau d'entitées */
 		void unsetmob();
 };

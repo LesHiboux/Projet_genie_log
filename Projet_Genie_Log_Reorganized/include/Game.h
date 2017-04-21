df174a0892e17258905c1dc112c40eda6574b3e1
MartinAlexandreCERI <martinalexandre0496@gmail.com>
2017-04-21 17:39:09 +0200

    Update Game.h

--- Projet_Genie_Log_Reorganized/include/Game.h
+++ Projet_Genie_Log_Reorganized/include/Game.h
@@ -9,13 +9,14 @@
 
 class Game
 {
-	Character joueur;
-	Map carte;
-	int nbRestant;
-	std::stack <Character> pileMob;
-	Affichage vue;
+	Character joueur;			// Le personnage du Joueur
+	Map carte;				// La carte sur laquelle il joue
+	int nbRestant;				// Le nombre de monstres restants
+	std::stack <Character> pileMob;		// La pile contenant les monstres restants
+	Affichage vue;				// L'affichage de toutes les informations
 
 	public:
+		//! Constucteur d'une partie
 		/*!
 			Constucteur de Game, initialise:
 				-Catre: Map
@@ -24,42 +25,30 @@ class Game
 		*/
 		Game();
 
-		/*!
-			Initialise la pile regroupant les mob
-		*/
+		//! Initialise la pile regroupant les mob
 		void setPileMob();
 
-		/*!
-			Initialise la Map
-		*/
+		//! Initialise la Map
 		Map getCarte();
 		
+		//! Initialise le Personnage du joueur,
 		/*!
-			Initialise le Personnage du joueur,
-			
 			\return true si bien initialisé,
 			\return false sinon
 		*/
 		bool selection_perso();
 
-		/*!
-			Récupère le nombre de mob à tuer pour gagner
-		*/
+		//! Récupère le nombre de mob à tuer pour gagner
 		int getNbRestant();
 
-		/*!
-			Récupère le joueur
-		*/
+		//! Récupère le joueur
 		Character getJoueur();
 		
-		/*!
-			Gèrer le déroulement d'une partie
-		*/
+		//! Gèrer le déroulement d'une partie
 		void partie();
 
+		//! Gère le combat
 		/*!
-			Gère le combat
-			
 			\param monstre Le monstre
 			
 			\return true: Le combat est gagné (le joueur n'est pas mort)
@@ -67,17 +56,16 @@ class Game
 		*/
 		bool combat(Character monstre);
 
-		/*!
-			Permet au joueur de choisir son sort
-		*/
+		//! Permet au joueur de choisir son sort
 		Skill selectSkillJoueur();
 
+		//! Permet au monstre de choisir sa compétence
 		/*!
-			Permet au monstre de choisir sa compétence
 			\param monstre Le monstre
 		*/
 		Skill selectSkillMonstre(Character monstre);
 
+		//! Gestion d'un tour
 		/*!
 			Gère un tour:
 				-calcul des priorités (le joueur ou le monstre joue en premier)
@@ -86,7 +74,7 @@ class Game
 			\param sortJoueur Le sort du joueur
 			\param monstre Le monstre
 			\param sortMonstre Le sort du monstre
-			\param victoire Viérifie si le combat est gagné
+			\param victoire Vérifie si le combat est gagné
 
 			\return true si le combat est fini (l'un des deux Characteur est mort)
 			\return false si le combat n'est pas fini

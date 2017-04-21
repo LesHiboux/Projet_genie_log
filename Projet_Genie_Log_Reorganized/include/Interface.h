f8747ee310b781e752473b64cbfe4f2da88abbf3
POISSON8 <poisson8.cf@gmail.com>
2017-04-21 17:33:26 +0200

    Update Interface.h

--- Projet_Genie_Log_Reorganized/include/Interface.h
+++ Projet_Genie_Log_Reorganized/include/Interface.h
@@ -11,16 +11,16 @@
 class Interface
 {
     public:
-        void affichePerso(Character );
-        void afficheMob(Character );
+        void affichePerso(Character ); //fini à tester
+        void afficheMob(Character ); // fini à tester
         void afficheMap(Map carte); // fini et testé
         void afficheSkill(Skill );
-        void afficheCombat(Character , Skill , Character);
+        void afficheCombat(Character , Skill , Character); //fini à tester
         void afficheCombatDetails(Character , Skill , Character , Skill );
-        void afficheWinF();
+        void afficheWinF(); //fini à tester
         void afficheWinC(Map carte); // fini à tester
-        void afficheLoose();
-        void afficheSelectP(Character , Skill );
+        void afficheLoose(); //fini à tester
+        void afficheSelectP(Character , Skill ); //fini à tester
 };
 
 #endif // INTERFACE_H

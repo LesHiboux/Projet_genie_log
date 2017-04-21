3ccd7af3f119266873038c69fc92aa154599a9c4
Thrudon <maximilienhugot@gmail.com>
2017-04-21 17:04:29 +0200

    Add files via upload

--- Projet_Genie_Log_Reorganized/include/Character.h
+++ Projet_Genie_Log_Reorganized/include/Character.h
@@ -4,34 +4,34 @@
 #define CHARACTER_H
 
 
-class Character
+class Character                                     // Personnage - Monstre
 {
     public:
-        Character();
-        Character(std::string);
-        virtual ~Character();
-        std::string getName();
-        std::pair <int, int> getLife();
-        int getMaxLife();
-        std::pair <int, int> getMana();
-        int getMaxMana();
-        int getNbSkills();
-        int getSpeed();
-        Skill getSkill(int);
-        bool isAlive();
-        bool available(Skill);
-        void editLife(int);
-        void editMana(int);
+        Character();                                // Ctor par défaut
+        Character(std::string);                     // Ctor à partir d'un String
+        virtual ~Character();                       // Dtor
+        std::string getName();                      // Renvoie le nom du Character
+        std::pair <int, int> getLife();             // Renvoie la paire <vie/max>
+        int getMaxLife();                           // Renvoie le max de Vie
+        std::pair <int, int> getMana();             // Renvoie la paire <mana/max>
+        int getMaxMana();                           // Renvoie le max de Mana
+        int getNbSkills();                          // Renvoie le nb de Compétences
+        int getSpeed();                             // Renvoie la Vitesse
+        Skill getSkill(int);                        // Renvoie un Skill particulier
+        bool isAlive();                             // Renvoie TRUE si le Character est toujours en vie
+        bool available(Skill);                      // Renvoie TRUE si le personnage a assez de Mana pour utiliser la Compétence
+        void editLife(int);                         // Modifie la Vie en fonction de la valeur donnée (valeur négative pour la restaurer)
+        void editMana(int);                         // Modifie la Mana en fonction de la valeur donnée (valeur négative pour la restaurer)
 
     protected:
 
     private:
-        std::string name;
-        Gauge HP;
-        Gauge MP;
-        int nbSkills;
-        Skill* skills;
-        int speed;
+        std::string name;                           // Nom du Character
+        Gauge HP;                                   // Jauge de Vie
+        Gauge MP;                                   // Jauge de Mana
+        int nbSkills;                               // Nombre de Compétences
+        Skill* skills;                              // Tableau de Compétences
+        int speed;                                  // Vitesse
 };
 
 #endif // CHARACTER_H

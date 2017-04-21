c8e0308f5cd2e1fec44c558cb05120773a44a436
POISSON8 <poisson8.cf@gmail.com>
2017-04-21 16:43:11 +0200

    Add files via upload

---
+++ Projet_Genie_Log_Reorganized/include/Interface.h
@@ -0,0 +1,26 @@
+#ifndef INTERFACE_H
+#define INTERFACE_H
+
+#include "../include/Map.h"
+#include "../include/Character.h"
+#include "../include/Gauge.h"
+#include "../include/Skill.h"
+#include "../include/Game.h"
+
+
+class Interface
+{
+    public:
+        void affichePerso(Character );
+        void afficheMob(Character );
+        void afficheMap(Map carte); // fini et testé
+        void afficheSkill(Skill );
+        void afficheCombat();
+        void afficheCombatDetails();
+        void afficheWinF();
+        void afficheWinC(Map carte); // fini à tester
+        void afficheLoose();
+        void afficheSelectP();
+};
+
+#endif // INTERFACE_H

7a63725d6db4352a79a4e1e898eeac6dc909fe66
POISSON8 <poisson8.cf@gmail.com>
2017-04-28 16:20:29 +0200

    Update Interface.h

--- Projet_Genie_Log_Reorganized/include/Interface.h
+++ Projet_Genie_Log_Reorganized/include/Interface.h
@@ -12,12 +12,13 @@ class Interface
         void afficheMob(Character);                         // fini à tester
         void afficheMap(Map carte);                         // fini et testé
         void afficheSkill(Character);                       //fini à tester
+        void afficheCombatDebut(Character, Caracter);       //fini à tester
         void afficheCombat(Character, Character);           //fini à tester
-        void afficheCombatDetails(Character, Skill);          //fini à tester
+        void afficheCombatDetails(Character, Skill);        //fini à tester
         void afficheWinF();                                 //fini à tester
         void afficheWinC(Map carte);                        // fini à tester
         void afficheLoose();                                //fini à tester
-        void afficheSelectP(Character);              //fini à tester
+        void afficheSelectP(Character);                     //fini à tester
 };
 
 #endif // INTERFACE_H
